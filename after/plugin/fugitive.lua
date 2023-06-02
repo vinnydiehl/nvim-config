44e2e748d9c167736051d6cfff4c48eea2b47b3f
vinnydiehl <vinny.diehl@gmail.com>
2023-06-02 10:33:52 -0400

    Add `git push` and `branch` mappings

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,21 @@
+local function branch_name()
+  local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+  if branch ~= "" then
+    return branch
+  else
+    return ""
+  end
+end
+
+
+vim.api.nvim_create_autocmd({"FileType", "BufEnter", "FocusGained"}, {
+  callback = function()
+    vim.b.branch_name = branch_name()
+  end
+})
+
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gl", "<cmd>Git log --graph --decorate --pretty=oneline --abbrev-commit<CR>")
+vim.keymap.set("n", "<leader>gpo", "<Cmd>Git push origin " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gpu", "<Cmd>Git push upstream " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gl", "<Cmd>Git log --graph --decorate --pretty=oneline --abbrev-commit<CR>")
+vim.keymap.set("n", "<leader>gb", ":Git branch ")
