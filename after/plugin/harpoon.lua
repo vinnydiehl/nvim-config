ce2c3c93fe5ce4ff695ff866a6ee56afe9c88be6
vinnydiehl <vinny.diehl@gmail.com>
2023-06-02 09:19:06 -0400

    Integrate Harpoon into Telescope

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,10 +1,16 @@
 local mark = require "harpoon.mark"
 local ui = require "harpoon.ui"
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<Leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<Leader><C-e>", ":Telescope harpoon marks<CR>")
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-b>", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<Leader><C-h>", function() mark.rm_file(1) end)
+vim.keymap.set("n", "<Leader><C-t>", function() mark.rm_file(2) end)
+vim.keymap.set("n", "<Leader><C-n>", function() mark.rm_file(3) end)
+vim.keymap.set("n", "<Leader><C-b>", function() mark.rm_file(4) end)
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require "telescope.builtin"
+
+require("telescope").load_extension("harpoon")
+
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
