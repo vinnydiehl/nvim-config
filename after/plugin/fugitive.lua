d36254596a0c32a46e33ad798699fdfc6bb0c8d0
vinnydiehl <vinny.diehl@gmail.com>
2023-06-03 23:03:25 -0400

    Add `git pull --rebase` binds

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -17,6 +17,8 @@ vim.api.nvim_create_autocmd({"FileType", "BufEnter", "FocusGained"}, {
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gpo", "<cmd>Git push origin " .. branch_name() .. "<CR>")
 vim.keymap.set("n", "<leader>gpu", "<cmd>Git push upstream " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gpro", "<cmd>Git pull --rebase origin " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gpru", "<cmd>Git pull --rebase upstream " .. branch_name() .. "<CR>")
 vim.keymap.set("n", "<leader>gl", "<cmd>Git log --graph --decorate --pretty=oneline --abbrev-commit<CR>")
 vim.keymap.set("n", "<leader>gb", ":Git branch ")
 vim.keymap.set("n", "<leader>ga", "<cmd>Git add .<CR>")
