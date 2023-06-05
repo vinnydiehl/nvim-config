f4dbac3d0a8417ca2e47a6c870a3ae425884561c
vinnydiehl <vinny.diehl@gmail.com>
2023-06-05 16:28:35 -0400

    Add `git push -f` binds

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -14,15 +14,22 @@ vim.api.nvim_create_autocmd({"FileType", "BufEnter", "FocusGained"}, {
 })
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
 vim.keymap.set("n", "<leader>gco", "<cmd>Git branch<CR>")
-vim.keymap.set("n", "<leader>gpo", "<cmd>Git push origin " .. branch_name() .. "<CR>")
-vim.keymap.set("n", "<leader>gpu", "<cmd>Git push upstream " .. branch_name() .. "<CR>")
-vim.keymap.set("n", "<leader>gpro", "<cmd>Git pull --rebase origin " .. branch_name() .. "<CR>")
-vim.keymap.set("n", "<leader>gpru", "<cmd>Git pull --rebase upstream " .. branch_name() .. "<CR>")
-vim.keymap.set("n", "<leader>gl", "<cmd>Git log --graph --decorate --pretty=oneline --abbrev-commit<CR>")
 vim.keymap.set("n", "<leader>gb", ":Git checkout -b ")
+
 vim.keymap.set("n", "<leader>ga", "<cmd>Git add .<CR>")
 vim.keymap.set("n", "<leader>gca", function()
   vim.cmd("Git add .")
   vim.cmd("Git commit")
 end)
+
+vim.keymap.set("n", "<leader>gpo", "<cmd>Git push origin " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gpof", "<cmd>Git push -f origin " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gpu", "<cmd>Git push upstream " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gpuf", "<cmd>Git push -f upstream " .. branch_name() .. "<CR>")
+
+vim.keymap.set("n", "<leader>gpro", "<cmd>Git pull --rebase origin " .. branch_name() .. "<CR>")
+vim.keymap.set("n", "<leader>gpru", "<cmd>Git pull --rebase upstream " .. branch_name() .. "<CR>")
+
+vim.keymap.set("n", "<leader>gl", "<cmd>Git log --graph --decorate --pretty=oneline --abbrev-commit<CR>")
