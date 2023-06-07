f10db885168784249c1c289ab7f89c45cbe58788
vinnydiehl <vinny.diehl@gmail.com>
2023-06-07 09:24:48 -0400

    Disable relative line numbers for insert mode/unfocused windows

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,11 +1,22 @@
 -- Delete trailing whitespace on save
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
   command = [[%s/\s\+$//e]]
 })
 
 -- Reload .Xresources on save
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { ".Xresources" },
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = ".Xresources",
   command = [[!xrdb ~/.Xresources]]
+})
+
+-- Disable relative line numbers for insert mode and unfocused windows
+vim.api.nvim_create_augroup("numbertoggle", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = [[if &nu && mode() != "i" | set rnu | endif]]
+})
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinEnter" }, {
+  pattern = "*",
+  command = [[if &nu | set nornu | endif]]
 })
