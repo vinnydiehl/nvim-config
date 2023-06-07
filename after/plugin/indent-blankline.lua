5d33024982c03b7eeb5ac4953c8dd2683398b8c0
vinnydiehl <vinny.diehl@gmail.com>
2023-06-07 05:52:58 -0400

    Darken indentation line

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -1,4 +1,6 @@
+vim.cmd [[highlight IndentBlanklineChar guifg=#393939 gui=nocombine]]
+
 require("indent_blankline").setup {
   show_first_indent_level = false,
-  use_treesitter = true
+  use_treesitter = true,
 }
