6db1257aacc80cd2698cd024892ec871a1c1c4ad
Vinny Diehl <vinny.diehl@gmail.com>
2023-10-24 21:09:51 -0700

    Update Telescope to 0.1.4

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -9,7 +9,7 @@ return require("packer").startup(function(use)
   -- Telescope
 
   use {
-    "nvim-telescope/telescope.nvim", tag = "0.1.1",
+    "nvim-telescope/telescope.nvim", tag = "0.1.4",
     requires = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-live-grep-args.nvim"
